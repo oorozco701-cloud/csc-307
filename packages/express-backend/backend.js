0d96d5d6e02df9bc0f07e003acf2a72463475d9b
Osbaldo Orozco <oorozc@calpoly.edu>
2026-04-13 20:43:04 -0700

    added route to get user by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -25,6 +25,9 @@ const findUserByName = (name) => {
   );
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
 
@@ -34,6 +37,18 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; 
+
+  let result = findUserById(id);
+
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
